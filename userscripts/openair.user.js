ff78ecc383fc6c7779d977ab37359f7ada6ad7b8
NL\jaron.barends <jaron.barends@valtech.nl>
2018-10-02 13:40:52 +0200

    working with auto opening notes, keyboard shortcuts for notes

--- userscripts/openair.user.js
+++ userscripts/openair.user.js
@@ -3,23 +3,43 @@
 // @namespace http://www.jaron.nl/misc/bookmarklets
 // @description Improvements on open air time sheet
 // @include https://www.openair.com/timesheet.pl?*
-// @version 0.1
-// ==/UserScript== 
+// @version 0.3
+// ==/UserScript==   
 
 (function() {
 
+
 	/**
-	* open notes when they're empty
+	* perform click on a dialog elm
+	* @returns {undefined}
+	*/
+	const clickDialogElm = function(selector) {
+		const dialogBlock = document.querySelector(`.dialogBlock`);
+		if (dialogBlock) {
+			dialogBlock.querySelector(selector).click();
+		}
+	};
+	
+
+	/**
+	* open notes when input has value and notes are empty
 	* @returns {undefined}
 	*/
 	const openNotesIfEmpty = function(e) {
-		console.log('open');
 		const input = e.currentTarget;
-		const notesTrigger = input.parentNode.querySelector('.tsDetailsAnchor');
-		console.log(notesTrigger);
-		notesTrigger.click();
-		const dialogBlock = document.querySelector(`.dialogBlock`);
-		console.log(dialogBlock);
+		if (input.value !== "") {
+			const notesTrigger = input.parentNode.querySelector('.tsDetailsAnchor');
+
+			notesTrigger.click();
+			const dialogBlock = document.querySelector(`.dialogBlock`);
+			if (dialogBlock) {
+				const textarea = dialogBlock.querySelector('[name="notes"]');
+				if (textarea.value) {
+					// we already have notes
+					dialogBlock.querySelector('.dialogCloseButton').click();
+				}
+			}
+		}
 	};
 	
 
@@ -29,12 +49,41 @@
 	* @returns {undefined}
 	*/
 	const openNotesOnBlur = function() {
-		console.log('open on', document.querySelectorAll('.timesheetInputHour'));
 		const inputs = Array.from(document.querySelectorAll('.timesheetInputHour'));
 		inputs.forEach((input) => {
 			input.addEventListener('blur', openNotesIfEmpty);
 		})
 	};
+
+
+	/**
+	* add listeners for esc, alt-s for dialog
+	* @returns {undefined}
+	*/
+	const addDialogKeyListeners = function() {
+		document.addEventListener('keyup', (e) => {
+			const dialogBlock = document.querySelector(`.dialogBlock`);
+			const keyCodes = {
+				enter: 13,
+				esc: 27,
+				s: 83
+			};
+			if (dialogBlock) {
+				const keyCode = e.keyCode;
+				if (keyCode === keyCodes.s && e.getModifierState('Alt')) {
+					// alt + S: save note
+					clickDialogElm('.dialogCloseAndSaveButton');
+				} else if (keyCode === keyCodes.enter && e.getModifierState('Control')) {
+					// ctrl + enter: OK (close without save)
+					clickDialogElm('.dialogOkButton');
+				} else if (keyCode === keyCodes.esc) {
+					// ctrl + enter: OK (close without save)
+					clickDialogElm('.dialogCloseButton');
+				}
+			}
+		});
+	};
+	
 
 
 	/**
@@ -50,6 +99,7 @@
 		} else {
 			// kick off initialisation
 			openNotesOnBlur();
+			addDialogKeyListeners();
 		}
 	};
 
