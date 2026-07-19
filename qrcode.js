db2228f90996662c4e81e80d5f9ec032e1aa3cae
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-07-19 10:53:25 +0200

    fix qr code alignment

--- qrcode.js
+++ qrcode.js
@@ -4,48 +4,54 @@ createOverlay();
 showOverlay();
 
 function createOverlay() {
-	const qrSrc = 'https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=' + encodeURIComponent(document.location.href);
-	const img = document.createElement('img');
-	img.src = qrSrc;
+  // qrserver.com is a service of https://goqr.me/api/
+  const qrSrc =
+    'https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=' +
+    encodeURIComponent(document.location.href);
+  const img = document.createElement('img');
+  img.src = qrSrc;
 
-	const overlayStyles = {
-		accentColor: 'red',
-		position: 'fixed',
-		zIndex: 1000000,
-		width: '100%',
-		height: '100%',
-		inset: 0,
-		textAlign: 'center',
-		backgroundColor: 'rgba(0,0,0,0.9)',
-	};
-	Object.assign(overlay.style, overlayStyles);
+  const overlayStyles = {
+    accentColor: 'red',
+    position: 'fixed',
+    zIndex: 1000000,
+    width: '100%',
+    height: '100%',
+    inset: 0,
+    textAlign: 'center',
+    display: 'flex',
+    alignItems: 'start',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.9)',
+  };
+  Object.assign(overlay.style, overlayStyles);
 
-	const imgStyles = {
-		marginTop: '100px',
-		width: 'auto',
-		height: 'auto',
-		padding: '20px',
-		background: 'white',
-	}
-	Object.assign(img.style, imgStyles);
+  const imgStyles = {
+    marginTop: '100px',
+    width: 'auto',
+    height: 'auto',
+    padding: '20px',
+    background: 'white',
+  };
+  Object.assign(img.style, imgStyles);
 
-	overlay.appendChild(img);
-};
+  overlay.appendChild(img);
+}
 
 function showOverlay() {
-	document.body.appendChild(overlay);
-	overlay.addEventListener('click', removeOverlay);
-	window.addEventListener('keyup', escListener);
-};
+  document.body.appendChild(overlay);
+  overlay.addEventListener('click', removeOverlay);
+  window.addEventListener('keyup', escListener);
+}
 
 function removeOverlay() {
-	overlay.removeEventListener('click', removeOverlay);
-	window.removeEventListener('keyup', escListener);
-	document.body.removeChild(overlay);
-};
+  overlay.removeEventListener('click', removeOverlay);
+  window.removeEventListener('keyup', escListener);
+  document.body.removeChild(overlay);
+}
 
 function escListener(e) {
-	if (e.keyCode === 27) {
-		removeOverlay();
-	}
-};
+  if (e.keyCode === 27) {
+    removeOverlay();
+  }
+}
